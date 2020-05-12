a41176bf1bce25b0c6cb8ddde69f78fff4dc1116
brynn.meeus <brynn.meeus@student.howest.be>
2020-05-12 15:11:33 +0200

    added base css and html no js functionality

---
+++ src/main/resources/webroot/assets/js/toggle.js
@@ -0,0 +1,23 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    document.querySelector("#joinRoom").addEventListener('click', toggle);
+    document.querySelector("#createRoom").addEventListener('click', toggle);
+}
+
+function toggle(e) {
+    let id = e.target.id;
+    let joinroom = document.querySelector("#join");
+    let createroom = document.querySelector("#create");
+
+    switch (id) {
+        case 'joinRoom': joinroom.classList.remove("hidden");
+        createroom.classList.add("hidden");break;
+        case 'createRoom': joinroom.classList.add("hidden");
+            createroom.classList.remove("hidden"); break;
+        default: joinroom.classList.add("hidden");
+            createroom.classList.add("hidden"); break;
+    }
+
+}
