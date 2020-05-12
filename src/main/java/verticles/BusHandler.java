2a968106f29c6d507b19485a3a7a8db2e95280a4
Jorne <jorne.suwier@student.howest.be>
2020-05-12 13:22:19 +0200

    CreateQuizroom,joinQuizroom

--- src/main/java/verticles/BusHandler.java
+++ src/main/java/verticles/BusHandler.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class BusHandler extends AbstractVerticle {
+    private EventBus bus;
     private Logger logger = Logger.getLogger(this.getClass().getName());
     private List<Quiz> quizzes = new ArrayList<>();
 
@@ -21,19 +22,17 @@ public class BusHandler extends AbstractVerticle {
         JsonObject content = data.getJsonObject("content");
         String type = data.getString("type");
         String user = data.getString("user");
-        int GID;
-        if (data.getInteger("GID") == null){
-            GID = 0;
-        }
-        else {
-            GID = data.getInteger("GID");
-        }
+        int GID = (data.getInteger("GID") == null) ? 0 : data.getInteger("GID");
         logger.info(GID + "this is teh session we looking for");
         switch (type) {
+            case "Create":
+                    GID = quizzes.size()-1;
+                    quizzes.add(new Quiz(vertx.eventBus(), vertx, GID));
+                    JsonObject object = new JsonObject()
+                    .put("user",user).put("GID",GID);
+                    bus.publish(Config.HANDLER_URL, object);
+                break;
             case "Connect":
-                if (quizzes.get(GID) == null){
-                    quizzes.add(new Quiz(vertx.eventBus(), vertx, quizzes.size()-1));
-                }
                 quizzes.get(GID).addPlayer(new Player(user));
                 logger.info(user + " Connected to the EB");
                 break;
@@ -58,8 +57,7 @@ public class BusHandler extends AbstractVerticle {
 
     @Override
     public void start() {
-        EventBus eb = vertx.eventBus();
-        quizzes.add(new Quiz(eb,vertx, 0));
-        eb.consumer(Config.HANDLER_URL, this::handle);
+        bus = vertx.eventBus();
+        bus.consumer(Config.HANDLER_URL, this::handle);
     }
 }
